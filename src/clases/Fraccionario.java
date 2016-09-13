2569075eaa6c32aff979853a343147331b0cfc36
“Jaime” <jtorres61@cuc.edu.co>
2016-09-13 18:27:20 -0500

    Validaciones y exepciones terminadas

--- src/clases/Fraccionario.java
+++ src/clases/Fraccionario.java
@@ -9,56 +9,60 @@ package clases;
  *
  * @author jaime
  */
-public class Fraccionario {
+public class Fraccionario { 
 
-    private double numerador;
-    private double denominador;
+    private int numerador;
+    private int denominador;
 
-    public Fraccionario(double numerador, double denominador) {
+    public Fraccionario(int numerador, int denominador) throws DenominadorCeroExeption {
         this.numerador = numerador;
         this.denominador = denominador;
+        if (denominador == 0) {
+            throw new DenominadorCeroExeption();
+        }
     }
 
-    public double getNumerador() {
+    public int getNumerador() {
         return numerador;
     }
 
-    public void setNumerador(double numerador) {
+    public void setNumerador(int numerador) {
         this.numerador = numerador;
     }
 
-    public double getDenominador() {
+    public int getDenominador() {
         return denominador;
     }
 
-    public void setDenominador(double denominador) {
+    public void setDenominador(int denominador) {
         this.denominador = denominador;
     }
 
-    public Fraccionario Suma(Fraccionario f2) {
-        double numerador = this.getNumerador() * f2.getDenominador() + this.getDenominador() * f2.getNumerador();
-        double denominador = this.getDenominador() * f2.getDenominador();
+    public Fraccionario Suma(Fraccionario f2) throws DenominadorCeroExeption {
+        int numerador = this.getNumerador() * f2.getDenominador() + this.getDenominador() * f2.getNumerador();
+        int denominador = this.getDenominador() * f2.getDenominador();
         Fraccionario f = new Fraccionario(numerador, denominador);
         return f;
     }
 
-    public Fraccionario Restar(Fraccionario f2) {
-        double numerador = this.getNumerador() * f2.getDenominador() - this.getDenominador() * f2.getNumerador();
-        double denominador = this.getDenominador() * f2.getDenominador();
+    public Fraccionario Restar(Fraccionario f2) throws DenominadorCeroExeption {
+        int numerador = this.getNumerador() * f2.getDenominador() - this.getDenominador() * f2.getNumerador();
+        int denominador = this.getDenominador() * f2.getDenominador();
         Fraccionario f = new Fraccionario(numerador, denominador);
         return f;
     }
 
-    public Fraccionario Multiplicacion(Fraccionario f2) {
-        double numerador = this.getNumerador() * f2.getNumerador();
-        double denominador = this.getDenominador() * f2.getDenominador();
+    public Fraccionario Multiplicacion(Fraccionario f2) throws DenominadorCeroExeption {
+        int numerador = this.getNumerador() * f2.getNumerador();
+        int denominador = this.getDenominador() * f2.getDenominador();
         Fraccionario f = new Fraccionario(numerador, denominador);
         return f;
     }
-    public Fraccionario Dividir(Fraccionario f2){
-        double numerador = this.getNumerador() * f2.getDenominador();
-        double denominador = this.getDenominador() * f2.getNumerador();
-        Fraccionario f = new Fraccionario(numerador,denominador);
+
+    public Fraccionario Dividir(Fraccionario f2) throws DenominadorCeroExeption {
+        int numerador = this.getNumerador() * f2.getDenominador();
+        int denominador = this.getDenominador() * f2.getNumerador();
+        Fraccionario f = new Fraccionario(numerador, denominador);
         return f;
     }
 
